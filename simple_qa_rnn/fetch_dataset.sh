fc91a0b04a80178d467e3d4e7884461eb4d2de7e
Jimmy Lin <jimmylin@uwaterloo.ca>
2017-08-29 14:21:09 -0400

    Nuking simple_qa_rnn since we have separate BuboQA repo. (#42)
    cf. https://github.com/castorini/BuboQA

--- simple_qa_rnn/fetch_dataset.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# download dataset and put it in data directory
-mkdir data
-pushd data
-wget https://www.dropbox.com/s/tohrsllcfy7rch4/SimpleQuestions_v2.tgz
-tar -xvzf SimpleQuestions_v2.tgz
-popd
