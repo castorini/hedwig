b21cb62bc08eacaea3ec4fb5af9e0dc351057c9f
Achyudh Ram <achyudhk@gmail.com>
2019-03-14 13:43:47 -0400

    Remove multiple input sequence models

--- common/evaluation.py
+++ common/evaluation.py
@@ -8,8 +8,6 @@ from .evaluators.reuters_evaluator import ReutersEvaluator
 from .evaluators.snli_evaluator import SNLIEvaluator
 from .evaluators.sts2014_evaluator import STS2014Evaluator
 from .evaluators.quora_evaluator import QuoraEvaluator
-from nce.nce_pairwise_mp.evaluators.trecqa_evaluator import TRECQAEvaluatorNCE
-from nce.nce_pairwise_mp.evaluators.wikiqa_evaluator import WikiQAEvaluatorNCE
 
 
 class EvaluatorFactory(object):
@@ -17,26 +15,12 @@ class EvaluatorFactory(object):
     Get the corresponding Evaluator class for a particular dataset.
     """
     evaluator_map = {
-        'sick': SICKEvaluator,
-        'msrvid': MSRVIDEvaluator,
         'SST-1': SSTEvaluator,
         'SST-2': SSTEvaluator,
-        'trecqa': TRECQAEvaluator,
-        'wikiqa': WikiQAEvaluator,
-        'pit2015': PIT2015Evaluator,
-        'twitterurl': PIT2015Evaluator,
         'Reuters': ReutersEvaluator,
         'AAPD': ReutersEvaluator,
         'IMDB': ReutersEvaluator,
-        'Yelp2014': ReutersEvaluator,
-        'SNLI': SNLIEvaluator,
-        'sts2014': STS2014Evaluator,
-        'Quora': QuoraEvaluator
-    }
-
-    evaluator_map_nce = {
-        'trecqa': TRECQAEvaluatorNCE,
-        'wikiqa': WikiQAEvaluatorNCE
+        'Yelp2014': ReutersEvaluator
     }
 
     @staticmethod
@@ -44,13 +28,12 @@ class EvaluatorFactory(object):
         if data_loader is None:
             return None
 
-        evaluator_map = EvaluatorFactory.evaluator_map_nce if nce else EvaluatorFactory.evaluator_map
         if not hasattr(dataset_cls, 'NAME'):
             raise ValueError('Invalid dataset. Dataset should have NAME attribute.')
 
-        if dataset_cls.NAME not in evaluator_map:
+        if dataset_cls.NAME not in EvaluatorFactory.evaluator_map:
             raise ValueError('{} is not implemented.'.format(dataset_cls))
 
-        return evaluator_map[dataset_cls.NAME](
+        return EvaluatorFactory.evaluator_map[dataset_cls.NAME](
             dataset_cls, model, embedding, data_loader, batch_size, device, keep_results
         )
--- nce/nce_pairwise_mp/trainers/trecqa_trainer.py
+++
@@ -1,7 +0,0 @@
-from nce.nce_pairwise_mp.trainers.qa_trainer import QATrainer
-
-
-class TRECQATrainerNCE(QATrainer):
-
-    def __init__(self, model, train_loader, trainer_config, train_evaluator, test_evaluator, dev_evaluator=None):
-        super(TRECQATrainerNCE, self).__init__(model, train_loader, trainer_config, train_evaluator, test_evaluator, dev_evaluator)
