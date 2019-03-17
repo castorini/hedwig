a870717efc24bb3c499f50ce417daa9062a2fe2b
Ashutosh-Adhikari <aashu.ad@gmail.com>
2019-03-17 17:20:44 -0400

    Change args.py for HAN and XMLCNN

--- models/han/args.py
+++ models/han/args.py
@@ -4,31 +4,19 @@ import os
 
 def get_args():
     parser = ArgumentParser(description="HAN")
-    parser.add_argument('--no_cuda', action='store_false', help='do not use cuda', dest='cuda')
-    parser.add_argument('--gpu', type=int, default=0) # Use -1 for CPU
-    parser.add_argument('--epochs', type=int, default=30)
-
-
     parser.add_argument('--word_num_hidden', type=int, default=50)
     parser.add_argument('--sentence_num_hidden', type=int, default=50)
 
     parser.add_argument('--single_label', action='store_true')
-    parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--mode', type=str, default='static', choices=['rand', 'static', 'non-static'])
-    parser.add_argument('--lr', type=float, default=1.0)
-    parser.add_argument('--seed', type=int, default=3435)
     parser.add_argument('--dataset', type=str, default='SST-1', choices=['SST-1', 'SST-2', 'Reuters', 'AAPD', 'IMDB', 'Yelp2014'])
     parser.add_argument('--resume_snapshot', type=str, default=None)
-    parser.add_argument('--log_every', type=int, default=10)
-    parser.add_argument('--patience', type=int, default=50)
     parser.add_argument('--save_path', type=str, default='han/saves')
     parser.add_argument('--output_channel', type=int, default=100)
     parser.add_argument('--words_dim', type=int, default=300)
     parser.add_argument('--embed_dim', type=int, default=300)
     parser.add_argument('--dropout', type=float, default=0.5)
     parser.add_argument('--epoch_decay', type=int, default=15)
-    parser.add_argument('--data_dir', help='word vectors directory',
-                        default=os.path.join(os.pardir, 'Castor-data', 'datasets'))
     parser.add_argument('--word-vectors-dir', default=os.path.join(os.pardir, 'Castor-data', 'embeddings', 'word2vec'))
     parser.add_argument('--word-vectors-file', default='GoogleNews-vectors-negative300.txt')
     parser.add_argument('--trained_model', type=str, default="")
--- models/xml_cnn/args.py
+++ models/xml_cnn/args.py
@@ -5,30 +5,18 @@ from argparse import ArgumentParser
 
 def get_args():
     parser = ArgumentParser(description="XML CNN")
-    parser.add_argument('--no_cuda', action='store_false', help='do not use cuda', dest='cuda')
-    parser.add_argument('--gpu', type=int, default=0, help='Use -1 for CPU')
-    parser.add_argument('--epochs', type=int, default=30)
-    parser.add_argument('--batch_size', type=int, default=1024)
     parser.add_argument('--mode', type=str, default='multichannel', choices=['rand', 'static', 'non-static', 'multichannel'])
-    parser.add_argument('--lr', type=float, default=1.0)
-    parser.add_argument('--seed', type=int, default=3435)
     parser.add_argument('--dataset', type=str, default='SST-1', choices=['SST-1', 'SST-2', 'Reuters','AAPD', 'IMDB', 'Yelp2014'])
     parser.add_argument('--resume_snapshot', type=str, default=None)
     parser.add_argument('--dev_every', type=int, default=30)
-    parser.add_argument('--log_every', type=int, default=10)
-    parser.add_argument('--patience', type=int, default=50)
     parser.add_argument('--save_path', type=str, default='xml_cnn/saves')
     parser.add_argument('--output_channel', type=int, default=100)
     parser.add_argument('--words_dim', type=int, default=300)
     parser.add_argument('--embed_dim', type=int, default=300)
     parser.add_argument('--dropout', type=float, default=0.5)
     parser.add_argument('--epoch_decay', type=int, default=15)
-
     parser.add_argument('--num_bottleneck_hidden', type=int, default=512) # bottleneck layer
     parser.add_argument('--dynamic_pool_length', type=int, default=32) # dynamic pool length
-
-    parser.add_argument('--data_dir', help='word vectors directory',
-                        default=os.path.join(os.pardir, 'Castor-data', 'datasets'))
     parser.add_argument('--word_vectors_dir', help='word vectors directory',
                         default=os.path.join(os.pardir, 'Castor-data', 'embeddings', 'word2vec'))
     parser.add_argument('--word_vectors_file', help='word vectors filename', default='GoogleNews-vectors-negative300.txt')
