bc3f76434e08db3206095c84ed7787eb78ac1cba
Achyudh Ram <achyudhk@gmail.com>
2019-03-17 20:11:55 -0400

    Fix char_cnn args

--- models/reg_lstm/args.py
+++ models/reg_lstm/args.py
@@ -6,18 +6,16 @@ import models.args
 def get_args():
     parser = models.args.get_args()
 
-    parser.add_argument('--resume-snapshot', type=str, default=None)
     parser.add_argument('--bidirectional', action='store_true')
     parser.add_argument('--bottleneck-layer', action='store_true')
     parser.add_argument('--num-layers', type=int, default=2)
     parser.add_argument('--hidden-dim', type=int, default=256)
     parser.add_argument('--mode', type=str, default='static', choices=['rand', 'static', 'non-static'])
-    parser.add_argument('--dataset', type=str, default='Reuters', choices=['SST-1', 'SST-2', 'Reuters', 'AAPD', 'IMDB', 'Yelp2014'])
+    parser.add_argument('--dataset', type=str, default='Reuters', choices=['Reuters', 'AAPD', 'IMDB', 'Yelp2014'])
     parser.add_argument('--words-dim', type=int, default=300)
     parser.add_argument('--embed-dim', type=int, default=300)
     parser.add_argument('--dropout', type=float, default=0.5)
     parser.add_argument('--epoch-decay', type=int, default=15)
-    parser.add_argument('--trained-model', type=str, default="")
     parser.add_argument('--tar', type=float, default=0.0, help="temporal activation regularization")
     parser.add_argument('--ar', type=float, default=0.0, help="activation regularization")
     parser.add_argument('--weight-decay', type=float, default=0)
@@ -28,6 +26,8 @@ def get_args():
     parser.add_argument('--word-vectors-dir', default=os.path.join(os.pardir, 'Castor-data', 'embeddings', 'word2vec'))
     parser.add_argument('--word-vectors-file', default='GoogleNews-vectors-negative300.txt')
     parser.add_argument('--save-path', type=str, default=os.path.join('reg_lstm', 'saves'))
+    parser.add_argument('--resume-snapshot', type=str, default=None)
+    parser.add_argument('--trained-model', type=str, default="")
 
     args = parser.parse_args()
     return args
