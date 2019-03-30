ee3d5abc03f1205e202e1e5c26c041959b3a78b3
Ashutosh-Adhikari <aashu.ad@gmail.com>
2019-03-30 15:15:37 -0400

    Return logits from the model.py (#12)

--- models/bert/model.py
+++ models/bert/model.py
@@ -973,13 +973,7 @@ class BertForSequenceClassification(BertPreTrainedModel):
         _, pooled_output = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
         pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
-
-        if labels is not None:
-            loss_fct = CrossEntropyLoss()
-            loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
-            return loss
-        else:
-            return logits
+        return logits
 
 
 class BertForMultipleChoice(BertPreTrainedModel):
