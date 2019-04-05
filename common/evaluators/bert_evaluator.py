51970b274cd32e0cd28552989e541d116b51c213
Achyudh Ram <achyudhk@gmail.com>
2019-04-05 18:45:02 -0400

    Add multilabel support in BertEvaluator

--- common/evaluators/bert_evaluator.py
+++ common/evaluators/bert_evaluator.py
@@ -1,30 +1,29 @@
 import os
 
+import numpy as np
 import torch
+import torch.nn.functional as F
+from sklearn import metrics
 from torch.utils.data import DataLoader, SequentialSampler, TensorDataset
 from tqdm import tqdm
 
-from datasets.processors.bert_processor import convert_examples_to_features, accuracy
-from utils.tokenization4bert import BertTokenizer
+from datasets.processors.bert_processor import convert_examples_to_features
+from utils.tokenization import BertTokenizer
 
 
 class BertEvaluator(object):
-    def __init__(self, model, processor, args):
+    def __init__(self, model, processor, args, split='dev'):
         self.args = args
         self.model = model
         self.processor = processor
-        self.eval_examples = self.processor.get_dev_examples(args.data_dir)
-        self.tokenizer = BertTokenizer.from_pretrained(args.bert_model, do_lower_case=args.do_lower_case)
+        self.tokenizer = BertTokenizer.from_pretrained(args.model, is_lowercase=args.is_lowercase)
+        if split == 'test':
+            self.eval_examples = self.processor.get_test_examples(args.data_dir)
+        else:
+            self.eval_examples = self.processor.get_dev_examples(args.data_dir)
 
     def evaluate(self):
-        label_list = self.processor.get_labels()
-        eval_features = convert_examples_to_features(self.eval_examples,
-                                                     label_list,
-                                                     self.args.max_seq_length,
-                                                     self.tokenizer)
-
-        print("Num. of examples =", len(self.eval_examples))
-        print("Batch size = %d", self.args.eval_batch_size)
+        eval_features = convert_examples_to_features(self.eval_examples, self.args.max_seq_length, self.tokenizer)
 
         all_input_ids = torch.tensor([f.input_ids for f in eval_features], dtype=torch.long)
         all_input_mask = torch.tensor([f.input_mask for f in eval_features], dtype=torch.long)
@@ -33,12 +32,13 @@ class BertEvaluator(object):
 
         eval_data = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
         eval_sampler = SequentialSampler(eval_data)
-        eval_dataloader = DataLoader(eval_data, sampler=eval_sampler, batch_size=self.args.eval_batch_size)
+        eval_dataloader = DataLoader(eval_data, sampler=eval_sampler, batch_size=self.args.batch_size)
 
         self.model.eval()
 
-        eval_loss, eval_accuracy = 0, 0
+        total_loss = 0
         nb_eval_steps, nb_eval_examples = 0, 0
+        predicted_labels, target_labels = list(), list()
 
         for input_ids, input_mask, segment_ids, label_ids in tqdm(eval_dataloader, desc="Evaluating"):
             input_ids = input_ids.to(self.args.device)
@@ -47,28 +47,42 @@ class BertEvaluator(object):
             label_ids = label_ids.to(self.args.device)
 
             with torch.no_grad():
-                tmp_eval_loss = self.model(input_ids, segment_ids, input_mask, label_ids)
                 logits = self.model(input_ids, segment_ids, input_mask)
 
-            logits = logits.detach().cpu().numpy()
-            label_ids = label_ids.to('cpu').numpy()
-            tmp_eval_accuracy = accuracy(logits, label_ids)
+            if self.args.is_multilabel:
+                predicted_labels.extend(F.sigmoid(logits).round().long().cpu().detach().numpy())
+                target_labels.extend(label_ids.cpu().detach().numpy())
+                loss = F.binary_cross_entropy_with_logits(logits, label_ids.float(), size_average=False)
+            else:
+                predicted_labels.extend(torch.argmax(logits, dim=1).cpu().detach().numpy())
+                target_labels.extend(torch.argmax(label_ids, dim=1).cpu().detach().numpy())
+                loss = F.cross_entropy(logits, torch.argmax(label_ids, dim=1))
 
-            eval_loss += tmp_eval_loss.mean().item()
-            eval_accuracy += tmp_eval_accuracy
+            if self.args.n_gpu > 1:
+                loss = loss.mean()
+            if self.args.gradient_accumulation_steps > 1:
+                loss = loss / self.args.gradient_accumulation_steps
+            total_loss += loss.item()
 
             nb_eval_examples += input_ids.size(0)
             nb_eval_steps += 1
 
-        eval_loss = eval_loss / nb_eval_steps
-        eval_accuracy = eval_accuracy / nb_eval_examples
+        predicted_labels, target_labels = np.array(predicted_labels), np.array(target_labels)
+        accuracy = metrics.accuracy_score(target_labels, predicted_labels)
+        precision = metrics.precision_score(target_labels, predicted_labels, average='micro')
+        recall = metrics.recall_score(target_labels, predicted_labels, average='micro')
+        f1 = metrics.f1_score(target_labels, predicted_labels, average='micro')
+        avg_loss = total_loss / nb_eval_steps
 
-        result = {'eval_loss': eval_loss,
-                  'eval_accuracy': eval_accuracy}
+        result = {'accuracy': accuracy,
+                  'precision': precision,
+                  'recall': recall,
+                  'f1': f1,
+                  'avg_loss': avg_loss}
 
-        output_eval_file = os.path.join(self.args.output_dir, "eval_results.txt")
+        output_eval_file = os.path.join(self.args.save_path, "eval_results.txt")
         with open(output_eval_file, "w") as writer:
             print("***** Eval results *****")
             for key in sorted(result.keys()):
-                print("  %s = %s", key, str(result[key]))
+                print(key, result[key])
                 writer.write("%s = %s\n" % (key, str(result[key])))
