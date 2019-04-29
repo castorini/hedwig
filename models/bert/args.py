0890eae1eedc47971f6d9d2f1ea4ea5e9f20810a
Achyudh Ram <achyudhk@gmail.com>
2019-04-29 02:12:08 -0400

    Remove unwanted args from models/bert

--- models/bert/args.py
+++ models/bert/args.py
@@ -1,5 +1,4 @@
 import os
-from argparse import ArgumentParser
 
 import models.args
 
@@ -11,6 +10,9 @@ def get_args():
     parser.add_argument('--dataset', type=str, default='SST-2', choices=['SST-2', 'AGNews', 'Reuters', 'AAPD', 'IMDB', 'Yelp2014'])
     parser.add_argument('--save-path', type=str, default=os.path.join('model_checkpoints', 'bert'))
     parser.add_argument('--cache-dir', default='cache', type=str)
+    parser.add_argument('--trained-model', default=None, type=str)
+    parser.add_argument('--local-rank', type=int, default=-1, help='local rank for distributed training')
+    parser.add_argument('--fp16', action='store_true', help='use 16-bit floating point precision')
 
     parser.add_argument('--max-seq-length',
                         default=128,
@@ -19,25 +21,22 @@ def get_args():
                              'Sequences longer than this will be truncated, and sequences shorter \n'
                              'than this will be padded.')
 
-    parser.add_argument('--trained-model', default=None, type=str)
-    parser.add_argument('--local-rank', type=int, default=-1, help='local rank for distributed training')
-    parser.add_argument('--fp16', action='store_true', help='use 16-bit floating point precision')
-
     parser.add_argument('--warmup-proportion',
                         default=0.1,
                         type=float,
                         help='Proportion of training to perform linear learning rate warmup for')
 
-    parser.add_argument('--gradient-accumulation-steps', type=int, default=1,
+    parser.add_argument('--gradient-accumulation-steps',
+                        type=int,
+                        default=1,
                         help='Number of updates steps to accumulate before performing a backward/update pass')
 
     parser.add_argument('--loss-scale',
-                        type=float, default=0,
+                        type=float,
+                        default=0,
                         help='Loss scaling to improve fp16 numeric stability. Only used when fp16 set to True.\n'
                              '0 (default value): dynamic loss scaling.\n'
                              'Positive power of 2: static loss scaling value.\n')
 
-    parser.add_argument('--server-ip', type=str, default='', help='Can be used for distant debugging.')
-    parser.add_argument('--server-port', type=str, default='', help='Can be used for distant debugging.')
     args = parser.parse_args()
     return args
