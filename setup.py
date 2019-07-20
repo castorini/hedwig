ff04385ec3bf5b2a941a6be769de959808837064
Jesse <jcahill225@gmail.com>
2019-07-19 21:53:24 -0400

    Update setup.py package list
    Fixed paths to include 'models/'

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(name='hedwig',
       version='1.0.0',
       description='PyTorch deep learning models for document classification',
-      packages=['char_cnn', 'han', 'kim_cnn', 'reg_lstm', 'xml_cnn'],
+      packages=['models/char_cnn', 'models/han', 'models/kim_cnn', 'models/reg_lstm', 'models/xml_cnn'],
       )
