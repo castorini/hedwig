7a1fce7386473be0bca94ff6d9e64f95ca0fb660
Achyudh Ram <7617287+achyudh@users.noreply.github.com>
2019-09-08 10:19:38 -0400

    Add MLP model (#37)
    *  Integrate BERT into Hedwig (#29)
    
    * Fix package imports
    
    * Update README.md
    
    * Fix bug due to TAR/AR attribute check
    
    * Add BERT models
    
    * Add BERT tokenizer
    
    * Return logits from the model.py
    
    * Remove unused classes in models/bert
    
    * Return logits from the model.py (#12)
    
    * Remove unused classes in models/bert (#13)
    
    * Add initial main file
    
    * Add args for BERT
    
    * Add partial support for BERT
    
    * Initialize training and optimization
    
    * Draft the structure of Trainers for BERT
    
    * Remove duplicate tokenizer
    
    * Add utils
    
    * Move optimization to utils
    
    * Add more structure for trainer
    
    * Refactor the trainer (#15)
    
    * Refactor the trainer
    
    * Add more edits
    
    * Add support for our datasets
    
    * Add evaluator
    
    * Split data4bert module into multiple processors
    
    * Refactor BERT tokenizer
    
    * Integrate BERT into Castor framework (#17)
    
    * Remove unused classes in models/bert
    
    * Split data4bert module into multiple processors
    
    * Refactor BERT tokenizer
    
    * Add multilabel support in BertTrainer
    
    * Add multilabel support in BertEvaluator
    
    * Add get_test_samples method in dataset processors
    
    * Fix args.py for BERT
    
    * Add support for Reuters, IMDB datasets for BERT
    
    * Revert "Integrate BERT into Castor framework (#17)"
    
    This reverts commit e4244ec73950d1efb15f706de6a4c77988c821ba.
    
    * Fix paths to datasets in dataset classes and args
    
    * Add SST dataset
    
    * Add hedwig-data instructions to README.md
    
    * Fix KimCNN README
    
    * Fix RegLSTM README
    
    * Fix typos in README
    
    * Remove trec_eval from README
    
    * Add tensorboardX to requirements.txt
    
    * Rename processors module to bert_processors
    
    * Add method to print metrics after training
    
    * Add model check-pointing and early stopping for BERT
    
    * Add logos
    
    * Update README.md
    
    * Fix code comments in classification trainer
    
    * Add support for AAPD, Sogou, AGNews and Yelp2014
    
    * Fix bug that deleted saved models
    
    * Update README for HAN
    
    * Update README for XML-CNN
    
    * Remove redundant TODOs from the READMEs
    
    * Fix logo in README.md
    
    * Update README for Char-CNN
    
    * Fix all the READMEs
    
    * Resolve conflict
    
    * Fix Typos
    
    * Re-Add SST2 Processor
    
    * Add support for evaluating trained model
    
    * Update args.py
    
    * Resolve issues due to DataParallel wrapper on saved model
    
    * Remove redundant Yelp processor
    
    * Fix bug for safely creating the saving directory
    
    * Change checkpoint paths to timestamps
    
    * Remove unwanted string.strip() from tokenizer
    
    * Create save path if it doesn't exist
    
    * Decouple model checkpoints from code
    
    * Remove model choice restrictions for BERT
    
    * Remove model/distill driver
    
    * Simplify checkpoint directory creation
    
    * Add TREC relevance datasets
    
    * Add relevance transfer trainer and evaluator
    
    * Add re-ranking module
    
    * Add ImbalancedDatasetSampler
    
    * Add relevance transfer package
    
    * Fix import in classification trainer
    
    * Remove unwanted args from models/bert
    
    * Fix bug where model wasn't in training mode every epoch
    
    * Add Robust45 preprocessor for BERT
    
    * Add support for BERT for relevance transfer
    
    * Add hierarchical BERT model
    
    * Remove tensorboardX logging
    
    * Add hierarchical BERT for relevance transfer
    
    * Add learning rate multiplier
    
    * Add lr multiplier for relevance transfer
    
    * Add MLP model

--- models/args.py
+++ models/args.py
@@ -8,7 +8,7 @@ def get_args():
     parser.add_argument('--no-cuda', action='store_false', dest='cuda')
     parser.add_argument('--gpu', type=int, default=0)
     parser.add_argument('--epochs', type=int, default=50)
-    parser.add_argument('--batch-size', type=int, default=1024)
+    parser.add_argument('--batch-size', type=int, default=32)
     parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--seed', type=int, default=3435)
     parser.add_argument('--patience', type=int, default=5)
---
+++ models/mlp/args.py
@@ -0,0 +1,20 @@
+import os
+
+import models.args
+
+
+def get_args():
+    parser = models.args.get_args()
+
+    parser.add_argument('--dataset', type=str, default='Reuters', choices=['Reuters', 'AAPD', 'IMDB', 'Yelp2014'])
+    parser.add_argument('--embed-dim', type=int, default=300)
+    parser.add_argument('--dropout', type=float, default=0.5)
+    parser.add_argument('--epoch-decay', type=int, default=15)
+    parser.add_argument('--weight-decay', type=float, default=0)
+
+    parser.add_argument('--save-path', type=str, default=os.path.join('model_checkpoints', 'mlp'))
+    parser.add_argument('--resume-snapshot', type=str)
+    parser.add_argument('--trained-model', type=str)
+
+    args = parser.parse_args()
+    return args
